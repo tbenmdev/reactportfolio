5657302514f14588c4b51fef91c6dccd4b5a98d2
tbenmedjbourdev <tbenmedjbourdev@gmail.com>
2023-03-28 20:33:58 -0400

    Created the Layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { Header } from "./components/Header";
+import { HeroArea } from "./components/HeroArea";
+import { About } from "./components/About";
+import { Services } from "./components/Services";
+import { Projects } from "./components/Projects";
+import { Activities } from "./components/Activities";
+import { Contacts } from "./components/Contacts";
+import { Footer } from "./components/Footer";
 
-function App() {
+import "./index.css";
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <HeroArea />
+      <h1 id="about" className="title">
+        About me
+      </h1>
+      <p className="description">My experience and education</p>
+      <About />
+      <h1 id="services" className="title">
+        My services
+      </h1>
+      <p className="description">How can I help you</p>
+      <Services />
+      <h1 id="projects" className="title">
+        Projects
+      </h1>
+      <p className="description">Highlights of my best work</p>
+      <Projects />
+      <h1 id="activities" className="title">
+        Activities
+      </h1>
+      <p className="description">My interests and volunteering activities</p>
+      <Activities />
+      <h1 id="contact" className="title">
+        Let's connect
+      </h1>
+      <p className="description">I'm available to work!</p>
+      <Contacts />
+      <Footer />
     </div>
   );
 }
-
-export default App;
